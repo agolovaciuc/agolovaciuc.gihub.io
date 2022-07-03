691e4bf6583a3379151c83fcec1bcc51178f6f66
agolovaciuc <93439634+agolovaciuc@users.noreply.github.com>
2022-07-03 18:05:13 +0300

    first commit
    start project

---
+++ js/main.js
@@ -0,0 +1,17 @@
+let name = "Andrew";
+let surName = "Golovaciuc"; 
+let age = 25;
+
+
+const personalData = {
+    name,
+    surName,
+    age
+}
+
+console.log(personalData['name'])
+
+
+console.log(`hello ${name}`);
+
+document.body.style.background = 'white';
